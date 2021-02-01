3f5ae744379ea1351645a483dfd02a86bb29a767
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2021-02-01 15:07:20 -0600

    unit test

---
+++ UnitTestFizzBuzz/UnitTestFizzBuzzPrint.cs
@@ -0,0 +1,31 @@
+using ClassLibraryStandard;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFizzBuzz
+{
+    [TestClass]
+    public class UnitTestFizzBuzzPrint
+    {
+        FizzBuzzPrint _print;
+
+
+        public UnitTestFizzBuzzPrint() 
+        {
+            _print = new FizzBuzzPrint();             
+        }
+
+        [TestMethod]
+        public void Three_Is_Fizz_True()
+        {
+
+            // arrange
+            string _IsThisFizz;
+
+            // act
+            _IsThisFizz = _print.Check(3);
+
+            // assert
+            Assert.IsTrue(_IsThisFizz == "Fizz");
+        }
+    }
+}
